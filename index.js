d1c9715e8ba4b7e1600e54651d71e1618d782943
lovecosma <williamsmatthew333@gmail.com>
2020-08-25 10:32:39 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+let cards = []
+function writeCards(array, string){
+  for(let i = 0; i < array.length; i++){
+    cards.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+  }
+return cards
+}
+
+
+function countDown(integer){
+  for (let i = integer; i >= 0; i--){
+    console.log(i)
+  }
+}
